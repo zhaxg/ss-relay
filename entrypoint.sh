2be13d65e2c6ade29a94add5d76a62fde0ef076a
zhaxg <zhaxg@qq.com>
2016-03-02 23:22:19 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
  
-sed -i 's/#server#/'"${SS-SERVER}"'/g' /usr/local/etc/haproxy/haproxy.cfg
-sed -i 's/#port#/'"${SS-PORT}"'/g' /usr/local/etc/haproxy/haproxy.cfg
-
+sed -i 's/#server#:#port#/'"${SS-SERVER}:${SS-PORT}"'/g' /usr/local/etc/haproxy/haproxy.cfg
 haproxy -f /usr/local/etc/haproxy/haproxy.cfg 
 
